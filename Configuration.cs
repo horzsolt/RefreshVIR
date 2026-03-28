b8895d3f4309b9a4e83f608b44485654a681a545
horzsolt <horzsolt2006@gmail.com>
2026-03-28 13:01:04 +0100

    Config refactor

---
+++ Configuration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefreshVIR
+{
+    public class Configuration
+    {
+        public static string connectionString = $"Server={Environment.GetEnvironmentVariable("VIR_SQL_SERVER_NAME")};" +
+                          $"Database=qad;" +
+                          $"User Id={Environment.GetEnvironmentVariable("VIR_SQL_USER")};" +
+                          $"Password={Environment.GetEnvironmentVariable("VIR_SQL_PASSWORD")};" +
+                          "Connection Timeout=500;Trust Server Certificate=true";
+
+        public static Dictionary<string, string> jobs = new Dictionary<string, string>
+            {
+                { "QAD_GL_frissites", "Főkönyv teljes frissítés" },
+                { "QAD_GL_INC_frissites", "Főkönyv növekményes frissítés" },
+                { "QAD_VIR_2025_ejszakai_frissites", "QAD 2025 frissítés 1." },
+                { "QAD_VIR_2025_ejszakai_frissites_2", "QAD 2025 frissítés 2." },
+                { "QAD_VIR_2025_ejszakai_frissites_3", "QAD 2025 frissítés 3." },
+                { "Refresh_Scriptor_1", "Scriptor frissítés 1." },
+                { "Refresh_Scriptor_2", "Scriptor frissítés 2." },
+                { "Refresh_Scriptor_3", "Scriptor frissítés 3." },
+                { "Refresh_Scriptor_4", "Scriptor frissítés 4." },
+                { "QAD_VIR_2025_ejszakai_frissites", "QAD 2025 frissítés 1." },
+                { "QAD_VIR_2025_ejszakai_frissites_2", "QAD 2025 frissítés 2." },
+                { "QAD_VIR_2025_ejszakai_frissites_3", "QAD 2025 frissítés 3." },
+            };
+    }
+}
--- MainForm.cs
+++ MainForm.cs
@@ -1,29 +1,7 @@
-using System.Data;
-using System.Diagnostics;
-using Microsoft.Data.SqlClient;
-
 namespace RefreshVIR
 {
     public partial class MainForm : Form
     {
-        string connectionString = $"Server={Environment.GetEnvironmentVariable("VIR_SQL_SERVER_NAME")};" +
-                          $"Database=qad;" +
-                          $"User Id={Environment.GetEnvironmentVariable("VIR_SQL_USER")};" +
-                          $"Password={Environment.GetEnvironmentVariable("VIR_SQL_PASSWORD")};" +
-                          "Connection Timeout=500;Trust Server Certificate=true";
-
-        Dictionary<string, string> jobs = new Dictionary<string, string>
-            {
-                { "QAD_GL_frissites", "Fõkönyv teljes frissítés" },
-                { "QAD_GL_INC_frissites", "Fõkönyv növekményes frissítés" },
-                { "QAD_VIR_2025_frissites", "QAD frissítés 1." },
-                { "QAD_VIR_2025_frissites_2", "QAD frissítés 2." },
-                { "QAD_VIR_2025_frissites_3", "QAD frissítés 3." },
-                { "Refresh_Scriptor_1", "Scriptor frissítés 1." },
-                { "Refresh_Scriptor_2", "Scriptor frissítés 2." },
-                { "Refresh_Scriptor_3", "Scriptor frissítés 3." },
-                { "Refresh_Scriptor_4", "Scriptor frissítés 4." }
-            };
 
         public MainForm()
         {
@@ -49,7 +27,7 @@ namespace RefreshVIR
 
         private async void btnRefreshGL_Click(object sender, EventArgs e)
         {
-            GLRefreshForm glForm = new GLRefreshForm(connectionString);
+            GLRefreshForm glForm = new GLRefreshForm(Configuration.connectionString);
             glForm.Show();
         }
 
@@ -60,7 +38,7 @@ namespace RefreshVIR
 
         private void button1_Click(object sender, EventArgs e)
         {
-            JobStatusForm jobForm = new JobStatusForm(connectionString, jobs);
+            JobStatusForm jobForm = new JobStatusForm(Configuration.connectionString, Configuration.jobs);
             jobForm.Show();
         }
     }
